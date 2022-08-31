812f429dc42e63e098e274041ebdbb210c6a5beb
lenovo <girikaustubh.sa@gmail.com>
2022-08-31 19:57:41 +0530

    Casting added

---
+++ src/primitiveCasting/ImplicitCatsing.java
@@ -0,0 +1,15 @@
+package primitiveCasting;
+
+public class ImplicitCatsing {
+
+	public static void main(String[] args) {
+
+		byte a=90;
+		System.out.println(a);
+		
+		double b=a;
+		System.out.println(b);
+			
+	}
+
+}
