d7a72f32df190a253d1b5d24e1e1f6b309fb1cf5
lenovo <kaustubh.giri2094@gmail.com>
2022-08-31 20:07:29 +0530

    Explicit casting added

---
+++ src/primitiveCasting/ExplicitCasting.java
@@ -0,0 +1,12 @@
+package primitiveCasting;
+
+public class ExplicitCasting {
+
+	public static void main(String[] args) {
+
+		System.out.println("higher data type info to lower data type info");
+		
+		
+	}
+
+}
