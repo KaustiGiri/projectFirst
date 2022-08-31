4e793cb1bb722beb3c2cc0753ca6741f8186d059
lenovo <kaustubh.giri2094@gmail.com>
2022-08-31 20:14:24 +0530

    add upcasting from non premitive

---
+++ src/nonPrimitiveCasting/Upcasting.java
@@ -0,0 +1,11 @@
+package nonPrimitiveCasting;
+
+public class Upcasting {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Refrance of parent class and object of child class");
+	}
+
+}
